7f8d02fa001ac37149948f28c5b2c02264efa463
Kristijan Federer <federer.kristijan@gmail.com>
2022-07-28 20:47:03 +0200

    providing the store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import store from "./store/index";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // connecting redux store with the react app
+  <Provider store={store}>
+    <App />
+  </Provider>
+);

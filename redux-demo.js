3a99bcd26123a2d98fc0b8ee2cde59de0c80f16e
Kristijan Federer <federer.kristijan@gmail.com>
2022-07-28 11:25:02 +0200

    dispatch created and assigned

--- redux-demo.js
+++ redux-demo.js
@@ -1,13 +1,13 @@
 const redux = require("redux");
 
-const counterReducer = (state, action) => {
+const counterReducer = (state = { counter: 0 }, action) => {
   return {
     counter: state.counter + 1,
   };
 };
 
 // Redux toolkit suggrst configStore instead of old createStore
-const store = redux.configStore(counterReducer);
+const store = redux.createStore(counterReducer);
 
 const counterSubscriber = () => {
   const latestState = store.getState();
@@ -16,3 +16,6 @@ const counterSubscriber = () => {
 
 // expects subscriber function (line 12)
 store.subscribe(counterSubscriber);
+
+// dispatches an action as a JS object
+store.dispatch({ type: 'increment' });

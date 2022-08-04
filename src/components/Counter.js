136487516b4ed2dfdab22db17034a754d353be79
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-04 13:45:07 +0200

    payload to actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,6 +10,10 @@ const Counter = () => {
     dispatch({ type: "increment" });
   };
 
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 5 })
+  }
+
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
@@ -21,6 +25,7 @@ const Counter = () => {
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <button onClick={incrementHandler}>Increment</button>
+      <button onClick={increaseHandler}>Increment by 5</button>
       <button onClick={decrementHandler}>Decrement</button>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>

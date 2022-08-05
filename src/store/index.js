288c74bd3652c670767dc059fa1f8cf3094b698c
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-05 12:05:41 +0200

    configureStore setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
 import {legacy_createStore as createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
 // createSlice preapres a slice of our global state
-createSlice({
+const counterSlice = createSlice({
   name: 'counter',
   initialState: initialState,
   reducers: {
@@ -23,39 +23,9 @@ createSlice({
   }
 })
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter
-    };
-  }
-
-  if (action.type === 'increase') {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter
-    };
-  }
-
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter
-    };
-  }
-
-  if (action.type === 'toggle') {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter
-    }
-  }
-
-  return state;
-};
-
 // Redux toolkit advices to replace configStore with createStore, works jsut with legacy_crateStore
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default store;
